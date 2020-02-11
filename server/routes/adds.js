34d23e0633b68651e8138930277751b6553125bf
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-02-11 22:23:18 +0530

    Feat: adds address api function

---
+++ server/routes/adds.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var data = require('../data/address')
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  if((data.address!=undefined) && (data.address.length>0))
+    res.status(200).send(data);
+  else
+    res.status(400).send("No data found");
+  //res.render('index', { title: 'Express' });
+});
+
+module.exports = router;  
