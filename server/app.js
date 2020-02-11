6fc0d69a8e06ce62b423f60797389d1a99a3f5a5
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-02-11 22:35:07 +0530

    Chore: adds cors and axiom package

--- server/app.js
+++ server/app.js
@@ -4,9 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var docsRouter = require('./routes/docs');
+var addsRouter = require('./routes/adds');
+
 
 var app = express();
 
@@ -14,6 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/docs', docsRouter);
+app.use('/adds', addsRouter);
 
 
 // catch 404 and forward to error handler
