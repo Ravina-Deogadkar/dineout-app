3c6c1cbd19442a9db15998000659d82d0a471000
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-06-14 22:26:17 +0530

    changed mapping

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Home from './component/home.js';
+import Home from './template/home.js';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 
