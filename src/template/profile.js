91eb753286a4d7fbf91de8c2577735966b60ee6a
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-08-13 20:05:52 +0530

    Feat: adds profile page with user details

---
+++ src/template/profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import userDetail from '../component/user-profile/dashboard/user-detail'
+
+export default class profile extends Component {
+    render() {
+        return (
+            <div>
+                <userDetail></userDetail>
+            </div>
+        )
+    }
+}
