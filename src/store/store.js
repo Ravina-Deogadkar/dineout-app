973f5727789a68ed2e2decfefc7636ae9a3299a2
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-08-16 14:04:21 +0530

    Feat: Adds store index page

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import {createStore, applyMiddleware} from "redux"
+import reducer from "./reducer/index"
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from 'redux-persist';
+
+
+
+const persistConfig = {
+    key: "cadroot",
+    storage,
+    // blackList: [reducer.misc, reducer.auth]
+};
+
+const reducers = persistReducer(persistConfig, reducer);
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+export const persistor = persistStore(store);
+
+export default store
