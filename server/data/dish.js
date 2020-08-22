de423694b33697972413155c5a52bcc488b9d425
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-08-22 22:24:22 +0530

    Feat: Adds dish mock data

---
+++ server/data/dish.js
@@ -0,0 +1,44 @@
+const dishes={
+    dishdata:[
+        {
+          hotelid:'H1',
+          dishid:1,
+          dishname:'Aloo Burger',
+          image:'',
+          category:"Snacks",
+          type:"Veg",
+          rating:3,
+          price:50,
+        },{
+            hotelid:'H1',
+            dishid:2,
+            dishname:'Chicken Burger',
+            image:'',
+            category:"Snacks",
+            type:"Veg",
+            rating:4,
+            price:90,
+          },
+          {
+            hotelid:'H2',
+            dishid:1,
+            dishname:'Biryani',
+            image:'',
+            category:"Main Course",
+            type:"Non Veg",
+            rating:5,
+            price:120,
+          },
+          {
+            hotelid:'H3',
+            dishid:1,
+            dishname:'Rasgulla',
+            image:'',
+            category:"Desert",
+            type:"Veg",
+            rating:4,
+            price:70,
+          }
+    ]
+}
+module.exports = dishes;
