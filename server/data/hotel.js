fb1b0b84517223e5ed285ee91892f16b198dc59c
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-08-22 22:26:05 +0530

    Feat: Adds hotel mock data

---
+++ server/data/hotel.js
@@ -0,0 +1,35 @@
+const hotel={
+    hoteldata:[
+        {
+          hotelid:'H1',
+          hotelname:'A Burger',
+          image:'',
+          category:["Snacks"],
+          type:"Veg",
+          location:"v",
+          rating:3,
+          avgprice:200,
+        },{
+            hotelid:'H2',
+          hotelname:'A Biryani',
+          image:'',
+          category:["Biryani","Chinese"],
+          type:["Veg","Non Veg"],
+          location:"v",
+          rating:4,
+          avgprice:400,
+          },
+          {
+            hotelid:'H3',
+          hotelname:'CSweets',
+          image:'',
+          category:["Desert"],
+          type:["Veg"],
+          location:"c",
+          rating:4.5,
+          avgprice:250,
+          },
+          
+    ]
+}
+module.exports = hotel;
