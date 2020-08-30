10020b01e96bd6bed54482a59ef784e63136f661
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-08-30 16:59:22 +0530

    Feat: adds funtions to get API data

---
+++ src/utils/fetchAPI.js
@@ -0,0 +1,36 @@
+import Axios from "axios";
+const serverURL="http://localhost:8030";
+
+export const fetchDishData = async () => {
+    const url =  serverURL+"/dish";
+
+    let data = await Axios.get(url);
+    const adsDetails = [];
+    console.log(data.data.dishdata);
+    if (data.data.dishdata != undefined) {
+        data.data.dishdata.map((value, index) => {
+            if (index < 6) {
+                adsDetails[index] = value;
+                //adsDetails[index].image = serverURL + value.image;
+            }
+        });
+    }
+    return adsDetails;
+};
+
+export const fetchHotelData = async () => {
+    const url =  serverURL+"/hotel";
+
+    let data = await Axios.get(url);
+    const adsDetails = [];
+    console.log(data.data.dishdata);
+    if (data.data.hoteldata != undefined) {
+        data.data.hoteldata.map((value, index) => {
+            if (index < 6) {
+                adsDetails[index] = value;
+                //adsDetails[index].image = serverURL + value.image;
+            }
+        });
+    }
+    return adsDetails;
+};
