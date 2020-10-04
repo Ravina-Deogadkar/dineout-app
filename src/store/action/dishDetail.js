cf7711f4977d2f47b23675bf2bdf01cf6bf789f1
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-10-04 17:08:52 +0530

    Feat: renamed cadDetail action

---
+++ src/store/action/dishDetail.js
@@ -0,0 +1,42 @@
+
+import {fetchDishData} from "../../utils/fetchAPI";
+
+export const getCad = (data) => {
+    return {
+      type: "SHOW_DISH",
+      value: data,
+    };
+  };
+
+  export const createCad = (data) => {
+    return {
+      type: "CREATE_DISH",
+      value: data,
+    };
+  };
+  export const addCart = (data) => {
+    return {
+      type: "ADD_TO_CART",
+      value: data,
+    };
+  };
+  // action creator
+
+  export const getCadData = (data) => {
+    return async (dispatch) => {
+      //let data=await fetchDishData();
+      dispatch(getCad(data));
+    };
+  };
+  export const createCadData = (data) => {
+    return async (dispatch) => {
+      
+      dispatch(createCad(data));
+    };
+  };
+  export const addToCart = (data) => {
+    return async (dispatch) => {
+      
+      dispatch(addCart(data));
+    };
+  };
