74e397a45b1c915703d0d16fee7e167db944efc1
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-10-25 19:07:04 +0530

    Refactor: combines reducers

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware} from "redux"
-import reducer from "./reducer/index"
+import {reducer} from "./reducer/index"
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import storage from "redux-persist/lib/storage";
