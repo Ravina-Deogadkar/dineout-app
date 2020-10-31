168b0d4544566cfe232e2ea02f0265df585c2c85
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-10-31 21:13:40 +0530

    Feat: Adds search component

--- src/component/search.js
+++ src/component/search.js
@@ -1,72 +1,56 @@
 import React, { Component } from 'react';
-import { makeStyles,withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Badge from '@material-ui/core/Badge';
-import SearchIcon from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
 
-const useStyles=(theme) => ({
-    root: {
-      maxWidth: 345,
-      
-    },
-    ttl:{
-        height: 48,
-    padding: '0 30px',
-    },
-    margin: {
-        margin: theme.spacing(1),
-      },
-      searchIcon: {
-        width: theme.spacing(7),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
+const useStyles = (theme) => ({
+	
+	ttl: {
+		height: 48,
+		padding: '0 30px',
+	},
+	margin: {
+		margin: theme.spacing(1),
+	},
+	
 })
 class Search extends Component {
-    
-    constructor(props){
-        super(props);
-        this.state={
-           /// expanded:false
-        }
-    }
 
-    render() {
-        const classes = this.props.classes;
-        console.log(this.props);
-        return (
-            <div>
-                <Typography variant="h6" noWrap className={classes.ttl} >
-                    Search
+	constructor() {
+		super();
+		this.state = {
+			search: '',
+		}
+	}
+
+	changeHandler = (event) => {
+		this.setState({ search: event.target.value})
+	}
+	clickHandler=()=>{
+		const { onSearch } = this.props;
+		onSearch(this.state.search);
+	}
+	render() {
+		const { classes } = this.props;
+		return (
+			<div>
+				<Typography variant="h6" noWrap className={classes.ttl} >
+					Search Dish
                 </Typography>
-                <div>
-                <TextField
-        className={classes.margin}
-        id="input-with-icon-textfield"
-        label=""
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Badge color="secondary">
-                <SearchIcon />
-                
-              </Badge>
-            </InputAdornment>
-          ),
-        }}
-      />
-                </div>   
-            </div>
-        )
-    }
+				<div>
+					<TextField
+						className={classes.margin}
+						id="input-with-icon-textfield"
+						label=""
+						onChange={this.changeHandler}
+					/>
+					<Button variant="contained" color="primary" onClick={this.clickHandler}>
+						Search
+      				</Button>
+				</div>
+			</div>
+		)
+	}
 }
 export default withStyles(useStyles)(Search)
