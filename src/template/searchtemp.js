7e989cb32f26b58ad4af9fee9b82f68fc5222029
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2020-11-07 21:10:54 +0530

    Feat: Adds search template

--- src/template/searchtemp.js
+++ src/template/searchtemp.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { getCadData } from "../store/action/dishDetail";
 import { makeStyles,withStyles } from '@material-ui/core/styles';
 import Search from '../component/search';
-import Layout from '../component/layout';
 import DineCard from '../component/dinecard';
-import Header from '../component/header';
-class Searchtemp extends Component {
-    render() {
-        return (
+import Grid from "@material-ui/core/Grid";;
 
+
+
+class Searchtemp extends Component {
+    constructor(props){
+        super(props);
+        console.log(this.props);
+        this.state={
+            dishdata: this.props.dishdata,
+        }
+    }
+    searchHandler=(search)=>{
+        console.log('search',search);
+        let statdata=this.state.dishdata;
+        let nstatdata=statdata.filter(item => item.dishname.toLowerCase().includes(search.toLowerCase()));
+        this.setState({ 'dishdata': nstatdata});
+    }
+
+    filterDishData=()=>{
+        
+    }
+    render() {
+        const { classes, dishdata } = this.props;
+        
+        return (
             <main>
-                <Search></Search>
-                <DineCard></DineCard>
+                <Search onSearch={this.searchHandler}></Search>
+                <div>
+                {this.state.dishdata.length>0?(
+                <Grid container style={{ margin: "5%", maxWidth: '90%' }} spacing={5}>
+                    {this.state.dishdata.map((value, index) => (
+                        <Grid item xs={3} sm={3} lg={4} key={index}>
+                            <DineCard dishdata={value}></DineCard>
+                        </Grid>
+                    ))}
+                </Grid>
+                ):<p>No data found</p>}
+                </div>
             </main>
         )
     }
 }
-export default (Searchtemp)
+
+const mapStateToProps = (state) => {
+    return {
+        dishdata: state.dishdata,
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getCadData_action: () => dispatch(getCadData()),
+    };
+};
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Searchtemp)
