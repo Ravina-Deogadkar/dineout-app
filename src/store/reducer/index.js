a89506b06dcb3ae9595bc9190d6ef3835397644f
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2021-02-13 20:32:37 +0530

    Feat: adds reducer in index

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -2,4 +2,5 @@ import { combineReducers } from '@reduxjs/toolkit';
 import dishdata from './dishDetail';
 import hotel from './hotelDetail';
 import cartdata from './cartDetail';
-export const reducer = combineReducers({ dishdata, hotel, cartdata});
+import offerdata from './offerDetail';
+export const reducer = combineReducers({ dishdata, hotel, cartdata, offerdata});
